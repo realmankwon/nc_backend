c686c1fbf9d74f9f6b13a7184f260ef9e94fa7ac
realmankwon <realman0214@gmail.com>
2024-09-13 15:14:06 +0900

    feat: modified process transaction

--- process_transaction.py
+++ process_transaction.py
@@ -1,19 +1,7 @@
 import json
 from datetime import timedelta, datetime
-from beem.block import Block
-from beem.amount import Amount
-import requests
 import time
-import requests
-import hashlib
-import ast
-import string
-import base36
-import random
-import websocket
-import sys
 
-from random import randint
 from utils.ncutils import checkifuser, findfreeplanet, shipdata, connectdb, get_shipdata,get_planetdata,get_distance,create_planet
 from utils.ncutils import find_starterplanet_coords, generateUid, uid_from_block, write_spacedb, get_ship_data, get_item_data, get_planet_data, update_transaction_status, update_transfer_status
 from utils.ncutils import get_mission_data, get_ask_data, read_parameter
